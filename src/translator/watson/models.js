df8433c0c3e148840409b46b9a6ef34f20db3923
Hitalo Emanoel <hitalo.emanoel@gmail.com>
2019-10-11 19:52:18 -0300

    more models added

--- src/translator/watson/models.js
+++ src/translator/watson/models.js
@@ -6,7 +6,21 @@ const models = {
         {src: 'English', target: 'German', model: 'en-de'},
         {src: 'English', target: 'Greek', model: 'en-el'},
         {src: 'English', target: 'Spanish', model: 'en-es'},
+        {src: 'English', target: 'Estonian', model: 'en-et'},
+        {src: 'English', target: 'Finnish', model: 'en-fi'},
+        {src: 'English', target: 'French', model: 'en-fr'},
+        {src: 'English', target: 'Hebrew', model: 'en-he'},
+        {src: 'English', target: 'Hindi', model: 'en-hi'},
+        {src: 'English', target: 'Croatian', model: 'en-hr'},
+        {src: 'English', target: 'Italian', model: 'en-it'},
+        {src: 'English', target: 'Japanese', model: 'en-ja'},
+        {src: 'English', target: 'Korean', model: 'en-ko'},
+        {src: 'English', target: 'Norwegian Bokmal', model: 'en-nb'},
+        {src: 'English', target: 'Dutch', model: 'en-nl'},
+        {src: 'English', target: 'Polish', model: 'en-pl'},
         {src: 'French', target: 'English', model: 'fr-en'},
+        {src: 'Portuguese', target: 'English', model: 'pt-en'},
+        {src: 'Spanish', target: 'English', model: 'es-en'},
     ]
 }
 
